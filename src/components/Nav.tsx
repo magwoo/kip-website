2689d7e1fc059ad8531978a60ed37edec7dc75e2
magwoo <magwood51@yandex.ru>
2025-01-14 11:57:26 +0300

    added page nav

---
+++ src/components/Nav.tsx
@@ -0,0 +1,40 @@
+import { JSX } from "solid-js";
+
+export function Nav() {
+  return (
+    <nav class="flex w-full justify-center bg-white/50 backdrop-blur-sm">
+      <div class="flex w-full max-w-screen-2xl justify-between px-12 py-3">
+        <div class="flex items-center gap-12">
+          <NavLink>Главная</NavLink>
+          <NavLink>Работы</NavLink>
+          <NavLink>Отчеты</NavLink>
+        </div>
+        <NavLink class="flex items-center gap-8">
+          <span>Бутин Роман</span>
+          <img
+            src="/assets/me-preview.png"
+            alt="roman-preview"
+            class="size-8"
+          />
+        </NavLink>
+      </div>
+    </nav>
+  );
+}
+
+type NavLinkProps = JSX.IntrinsicElements["a"];
+
+export function NavLink(props: NavLinkProps) {
+  return (
+    <a
+      {...props}
+      href={props.href ?? "#"}
+      class={[
+        "font-medium text-stone-900/50 hover:text-stone-900",
+        props.class,
+      ].join(" ")}
+    >
+      {props.children}
+    </a>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
+import { Nav } from "@components/Nav";
 import { A } from "@solidjs/router";
 
 export function Index() {
   return (
-    <section class="mt-64 flex flex-col items-center gap-4">
-      <h1 class="text-4xl font-medium">3ИСИП-722</h1>
-      <h2 class="text-xl opacity-50">тема: Смартфоны</h2>
-    </section>
+    <>
+      <Nav />
+      <section class="mt-64 flex flex-col items-center gap-4">
+        <h1 class="text-4xl font-medium">3ИСИП-722</h1>
+        <h2 class="text-xl opacity-50">тема: Смартфоны</h2>
+      </section>
+    </>
   );
 }
