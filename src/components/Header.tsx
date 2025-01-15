40fbd37326514fb64ba199002ba270f8cfd123b0
magwoo <magwood51@yandex.ru>
2025-01-15 09:55:55 +0300

    small fixes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,7 +21,7 @@ export function Header() {
     <header class="mt-24 flex w-full max-w-screen-xl justify-between gap-24 px-4">
       <div class="flex flex-col gap-16">
         <div class="flex flex-col gap-2">
-          <h1 class="text-7xl font-semibold text-stone-800">3ИСИП-322</h1>
+          <h1 class="text-7xl font-semibold text-stone-800">3ИСИП-722</h1>
           <h2 class="text-5xl font-medium text-stone-800/60">Смартфоны</h2>
         </div>
         <div class="flex flex-col items-start gap-10">
@@ -60,7 +60,7 @@ export function WorkItem(props: WorkItemProps) {
       <span class="select-none text-stone-800/25">|</span>
       <a
         href={props.href}
-        title="перейти к работе"
+        title="перейти"
         class="font-medium transition-colors hover:text-stone-800/75"
       >
         {props.children}
