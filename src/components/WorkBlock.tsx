de7c11c44977e3fb3bd13672d607471fec0de668
magwoo <magwood51@yandex.ru>
2025-01-17 12:18:27 +0300

    added work references animations

--- src/components/WorkBlock.tsx
+++ src/components/WorkBlock.tsx
@@ -19,22 +19,22 @@ export function WorkBlock(props: WorkBlockProps) {
           <h3 class="text-3xl font-semibold text-stone-800/75">{work.title}</h3>
           <p>{work.description}</p>
         </div>
-        <div class="flex flex-col gap-1">
+        <div class="flex flex-col gap-2">
           <span class="font-medium">Референсы</span>
           <div class="flex items-center gap-3">
-            <img
+            <ReferenceItem
               src="/assets/wildberries-icon.png"
               alt="wildberries-icon"
               class="size-12 rounded-lg"
             />
             <span class="text-xl text-stone-800/25">|</span>
-            <img
+            <ReferenceItem
               src="/assets/ozon-icon.png"
               alt="ozon-icon"
               class="size-12 rounded-lg"
             />
             <span class="text-xl text-stone-800/25">|</span>
-            <img
+            <ReferenceItem
               src="/assets/aliexpress-icon.png"
               alt="aliexpress-icon"
               class="size-12 rounded-lg"
@@ -56,6 +56,17 @@ export function WorkBlock(props: WorkBlockProps) {
           </button>
         </div>
       </div>
+    </div>
+  );
+}
+
+type ReferenceItemProps = JSX.IntrinsicElements["img"];
+
+export function ReferenceItem(props: ReferenceItemProps) {
+  return (
+    <div class="group relative size-12 cursor-pointer transition-all">
+      <img {...props} class="relative z-10 size-12 rounded-lg" />
+      <div class="absolute inset-0 rounded-xl bg-black/10 opacity-0 transition-all duration-200 group-hover:-inset-1 group-hover:opacity-100" />
     </div>
   );
 }
